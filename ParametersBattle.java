13d1913ebc64aa73b2dada774fbeae15da112cfd
MariagiovannaRotundo <mariagiovannarotundo@gmail.com>
2020-01-17 17:01:36 +0100

    Create ParametersBattle.java

---
+++ ParametersBattle.java
@@ -0,0 +1,41 @@
+
+
+public class ParametersBattle {
+
+	private static int K, X, Y, Z, T1, T2;
+	
+	
+	public ParametersBattle() {
+		K=8;
+		X=2;
+		Y=-1;
+		Z=4;
+		T1=10000;
+		T2=60000;
+	}
+	
+	public static int getK(){
+		return K;
+	}
+	
+	public static int getX(){
+		return X;
+	}
+	
+	public static int getY(){
+		return Y;
+	}
+	
+	public static int getZ(){
+		return Z;
+	}
+	
+	public static int getT1(){
+		return T1;
+	}
+	
+	public static int getT2(){
+		return T2;
+	}
+
+}
